c76b938b0b26fb98a15d15b1fc93a3b2c90d2fc2
gly <274473435@qq.com>
2022-05-24 18:35:26 +0800

    完成数据库操作

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [UserModule,TypeOrmModule.forRoot({
@@ -14,7 +15,7 @@ import { UserModule } from './user/user.module';
     database:'chart_app',
     autoLoadEntities:true, // 自动加载模块，而不是指定实体数组
     synchronize:true // TypeORM实体每次运行应用程序时自动同步数据库，生产环境需要关闭。
-  })],
+  }), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
